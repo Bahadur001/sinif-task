c281bc55bb76e5ce3749851a7410ff1d217c599d
Bahadur001 <161586965+Bahadur001@users.noreply.github.com>
2024-03-28 16:03:41 +0400

    commit

---
+++ app.js
@@ -0,0 +1,45 @@
+1.
+// let numbers = [1, 3, 5, 7, 9, 2, 8, 6, 4, 0];
+// const maxNumber = Math.max(...numbers);
+// console.log(maxNumber); 
+
+2.
+//   let numbers = [1, 3, 5, 7, 9, 2, 8, 6, 4, 13, 41, 44];
+//   let sadeededler = numbers.filter(num => num > 1 && num % 1  === 0);
+//   console.log(sadeededler);
+
+// let data = [
+//     "Salam",
+//     "Dünyalı",
+//     "JavaScript",
+//     "Programlaşdırma",
+//     "Kod",
+//     "Frontend",
+//     "Backend"
+//  ];
+
+//  let herf = data.filter(function(metin) {
+//      return metin.length === 7;
+//  });
+ 
+//  console.log(" 7 herfli olanlar:", herf);
+
+
+
+
+3.
+// let boy = prompt("boyunuz")
+// let kutle = prompt("kilonuz")
+// let index = (kutle/boy**2)
+// console.log("index:")
+
+// if (index < 18) {
+//     console.log("İdeal çəkidən aşağ:");
+// } else if (index >= 18 && index < 25) {
+//     console.log("İdeal çək");
+// } else if (index >= 25 && index < 30) {
+//     console.log(" İdeal çəkidən yuxarı ");
+// } else {
+//     console.log("Obez");
+// }
+
